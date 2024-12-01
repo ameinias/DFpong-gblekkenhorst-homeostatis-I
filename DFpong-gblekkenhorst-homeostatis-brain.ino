ebd89e3308581f3b98d0b0ada42628a4b63f3136
ameinias <gillian@blekkenhorst.ca>
2024-11-30 21:04:50 -0500

    commentary added

--- DFpong-gblekkenhorst-homeostatis-brain.ino
+++ DFpong-gblekkenhorst-homeostatis-brain.ino
@@ -1,9 +1,12 @@
 /*********************************************************************
- * DF Pong Controller
+ * DF Pong Controller - homeostasis BRAIN
  * 
  * This program implements a Bluetooth Low Energy controller for Pong.
  * It sends movement data to a central device running in the browser and
- * provides audio feedback through a buzzer.
+ * provides audio feedback through a buzzer. It uses a pressure sensor to 
+ * collect data - it takes the starting pressure and calculates threshold 
+ * above and below that value to decide how much pressure is required to 
+ * switch between Up/Down/Still. 
  *
  * Game Link : https://digitalfuturesocadu.github.io/df-pong/
  * 
@@ -13,91 +16,67 @@
  * 2 = DOWN movement (paddle moves down)
  * 3 = Handshake signal (used for initial connection verification)
  * 
- * Key Functions:
- * - handleInput(): Process the inputs to generate the states
- * - sendMovement(): Sends movement data over BLE (0-3)
- * - updateBLE(): Handles BLE connection management and updates
- * - updateBuzzer(): Provides different buzzer patterns for different movements
- * 
- * Key Variables:
- * - currentMovement: Stores current movement state (0-2)
- * - deviceName : GIVE YOUR DEVICE AN APPROPRIATE NAME
- * - LED_PIN : It is important to see the status of the arduino through the LED. 
-      if you can see the built-in add an external one and update the pin it is connected to
- * 
-
  *********************************************************************/
 
 
-
+// libraries
 #include <ArduinoBLE.h>
 #include "ble_functions.h"
 #include "buzzer_functions.h"
-//Since code is split over multiple files, we have to include them here
 
 
-//Name your controller!
+//Name of the controller
 const char* deviceName = "meat";
 
-bool startValueNil = true;
-int weightOffset = 50;
+bool startValueNil = true;  // If true, controller will calibrate STAY to the starting pressure value. 
+int weightOffset = 50;      // if startValueNil, will recalculate up and down threshold based on calibration
 
-// Pin definitions buzzer/LED
+// Pin definitions
 const int BUZZER_PIN = 11;    // Pin for haptic feedback buzzer
-const int LED_PIN_BLE = 7;    // Status LED pin
-const int LED_PIN_POWER = 2;  // Status LED pin
+const int LED_PIN_BLE = 7;    // Status LED pin, also indicated end of calibration period 
+const int sensorPin = A0;  // sensor pin
 
 // Movement state tracking
 int currentMovement = 0;  // Current movement value (0=none, 1=up, 2=down, 3=handshake)
 
 
-// Pin definitions for Button input
-const int BUTTON_UP_PIN = 6;    // Pin for UP movement button
-const int BUTTON_DOWN_PIN = 2;  // Pin for DOWN movement button
-
-// use button up pin only
-
-// variables:
+// sensor variables:
 int sensorValue = 0;       // the sensor value
 int sensorMin = 0;         // minimum sensor value
 int sensorMax = 1023;      // maximum sensor value
-const int sensorPin = A0;  // sensor pin
 
 //threshholds
-int downThreshold = 70;
-int upThreshold = 300;
+int stillThreshold;      // Holds the starting pressure during calibration  
+int downThreshold = 30;  // highest value for "DOWN", used if !startValueNil
+int upThreshold = 120;   // lowest value for "UP", used if !startValueNil
 
+// How frequently to check for input
 int interval = 5000;
-
-
 
 
 void setup() {
 
   Serial.begin(9600);
 
-  // Configure button pins with internal pullup resistors
-  // Buttons will read LOW when pressed, HIGH when released
-  pinMode(BUTTON_UP_PIN, INPUT_PULLUP);    // UP button configuration
-  pinMode(BUTTON_DOWN_PIN, INPUT_PULLUP);  // DOWN button configuration
 
   // Configure LED for connection status indication
   pinMode(LED_PIN_BLE, OUTPUT);
-
-  // Don't mess with the stuff below!
 
   // Initialize Bluetooth Low Energy with device name and status LED
   setupBLE(deviceName, LED_PIN_BLE);
 
   // Initialize buzzer for feedback
   setupBuzzer(BUZZER_PIN);
+
+  // Calibrate sensors to find up/down/still thresholds
   calibrateSensorStart();
 
-  debugText() ;
+  debugText();
 }
 
-void calibrateSensorStart() {
   // calibrate during the first five seconds - From Arduino examples
+void calibrateSensorStart() {
+
   while (millis() < 5000) {
     sensorValue = analogRead(sensorPin);
 
@@ -106,8 +85,7 @@ void calibrateSensorStart() {
       sensorMax = sensorValue;
     }
 
-
-
+    // check to see if the thresholds should be recalculated dynamically, otherwise run regular calibration
     if (startValueNil) {
       CalculateStartPositions();
     } else {
@@ -117,13 +95,11 @@ void calibrateSensorStart() {
       }
     }
 
-    // signal the end of the calibration period
-    digitalWrite(LED_PIN_POWER, HIGH);
   }
 }
 
-int stillThreshold;
 
+// recalculate thresholds dynamically from calibrated 'start' pressure
 void CalculateStartPositions() {
   stillThreshold = sensorValue;
   downThreshold = stillThreshold - 10;
@@ -136,7 +112,6 @@ void sensorInput() {
 
   // in case the sensor value is outside the range seen during calibration
   sensorValue = constrain(sensorValue, sensorMin, sensorMax);
-  //  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 2000);
 }
 
 void loop() {
@@ -148,33 +123,17 @@ void loop() {
 
   //read the inputs te determine the current state
   //results in changing the value of currentMovement
-  handleInputStatic();
-  //handleInputDynamic();
+  handleInput();
 
   //send the movement state to P5
   sendMovement(currentMovement);
 
-  updateBuzzer(2);
-
-}
-int diff;
-
-// not using this - doesn't work for gameplay.
-void handleInputDynamic() {
-
-  unsigned long currentMillis = millis();  // grab current time
-
-  diff = currentMillis - previousMillis;
-  if (currentMillis - previousMillis >= 1000) {
-
-    handleInputStatic();
-    Serial.println(diff);
-
-    previousMillis = millis();
-  }
-  Serial.println(diff);
+  // send movement state to the buzzer
+  updateBuzzer(currentMovement);
 }
 
+
+// prints calibration data after startup
 void debugText() {
   Serial.print("min: ");
   Serial.print(sensorMin);
@@ -184,13 +143,11 @@ void debugText() {
   Serial.println(sensorValue);
 }
 
+// prints additional sensor information - static for each playthrough, but changes on recalibration 
 void sensorRead() {
-
-
   Serial.print(" still: ");
   Serial.print(stillThreshold);
   Serial.print("                   ");
-
   Serial.print(" d ");
   Serial.print(downThreshold);
   Serial.print(" u: ");
@@ -199,41 +156,19 @@ void sensorRead() {
   Serial.print(sensorValue);
   Serial.print(" ");
 }
-void handleInputStatic() {
 
-
+void handleInput() {
   if (sensorValue > upThreshold) {
     currentMovement = 1;  // UP movement
     sensorRead();
     Serial.println("UP");
-     digitalWrite(LED_PIN_POWER, HIGH);
   } else if (sensorValue < downThreshold) {
     currentMovement = 2;  // DOWN movement
     sensorRead();
     Serial.println("DOWN");
-     digitalWrite(LED_PIN_POWER, HIGH);
   } else {
     currentMovement = 0;  // No movement
     sensorRead();
     Serial.println("stay");
-    
-  }
-}
-
-
-
-
-void handleInputOld() {
-  //flipped read method because of INPUT_PULLUP
-  bool upPressed = !digitalRead(BUTTON_UP_PIN);
-  bool downPressed = !digitalRead(BUTTON_DOWN_PIN);
-
-  if (upPressed && !downPressed) {
-    currentMovement = 1;
-    Serial.println(1);  // UP movement
-  } else if (downPressed && !upPressed) {
-    currentMovement = 2;  // DOWN movement
-  } else {
-    currentMovement = 0;  // No movement
   }
 }
